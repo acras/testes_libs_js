3ff467fc99492140bb64c5e9164902bbbd8b1fe9
Ricardo Acras <ricardo@acras.com.br>
2013-01-11 16:46:56 -0200

    scaffolded app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestesLibsJs::Application.config.secret_token = '1d3a10ede0d02f414ae0da2026ea1fc933ff7a7452b931dda8f83f3615fc9e85278f20d9c1fb2b8bde6918b8271cf2cf67099e6c15821a97c405e9a114118856'
